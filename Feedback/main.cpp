d2f7cc604c18b9946c93bf279bfee2942198608f
andrew <andrewshipilo@mail.ru>
2018-05-14 23:32:00 +0300

    CMake and formulas

--- Feedback/main.cpp
+++ Feedback/main.cpp
@@ -117,10 +117,10 @@ float UsageInReturnAlgorithm(float p, float tau, int64_t numberOfMessages)
 	int64_t cycle = 0;
 	int64_t cycleSum = 0;
 
-	bool receipt = false;
 	bool wasError = false;
 	int  lostMsgNum = 0;
 	int  tmpMsg = 0;
+
 	for (int message = 1;;)
 	{
 		// Simulating error
@@ -129,7 +129,6 @@ float UsageInReturnAlgorithm(float p, float tau, int64_t numberOfMessages)
 			std::cout << message << "|\t";
 			std::cout << "~\t|x" << std::endl;
 			lostMsgNum++;
-			cycle++;
 		}
 		else
 		{
@@ -143,20 +142,20 @@ float UsageInReturnAlgorithm(float p, float tau, int64_t numberOfMessages)
 			{
 				// If was error then send "-"
 				std::cout << "e\t|x" << std::endl;
-				receipt = false;
 				wasError = true;
 				lostMsgNum = 0;
 				tmpMsg = message;
+                cycleSum += tau + 1;
 			}
 			else
 			{
-				cycleSum += cycle++;
+			    cycleSum++;
+
 				std::cout << "ok\t|" << message << " T: " << cycle << std::endl;
 
 				if (message == numberOfMessages) { break; }
 				cycle = 0;
 				wasError = false;
-				receipt = true;
 			}
 		}
 
@@ -164,26 +163,46 @@ float UsageInReturnAlgorithm(float p, float tau, int64_t numberOfMessages)
 		tries++;
 	}
 
-	return (numberOfMessages / (float)tries) / (cycleSum / numberOfMessages);
+	return numberOfMessages / (float) cycleSum;
 }
 
 int main()
 {
 	/* 2.1 Среднее число передач в алгоритме с ожиданием при неограниченном числе повторных передач */
-	
-	std::cout << "Waiting, unlimited:\t\t" << AvgInWaitingAlgorithm(0.8f, 100000) << std::endl;
-	
-	/* 2.2 Среднее число передач в алгоритме с ожиданием при ограниченном числе повторных передач */
+	float p1 = 0.8f;
 
-	std::cout << "Waiting, limited:\t\t" << AvgInWaitingAlgorithm(0.8f, 100000, 2) << std::endl;
+	std::cout << "Waiting, unlimited:\t\t" << AvgInWaitingAlgorithm(p1, 100000) << std::endl;
+	std::cout << "Should be:\t\t" << 1 / (1 - p1) << std::endl;
+
+	/* 2.2 Среднее число передач в алгоритме с ожиданием при ограниченном числе повторных передач */
+    float p2 = 0.8f;
+    float n2 = 2;
+	std::cout << "Waiting, limited:\t\t" << AvgInWaitingAlgorithm(p2, 100000, n2) << std::endl;
+	std::cout << "Should be:\t\t" << (1 - std::pow(p2, n2)) / (1 - p2) << std::endl;
 
 	/* 2.3 Среднее число передач в алгоритме с ожиданием при наличии ошибок в обратном канал с неогр. и огр. повт. передач*/
+    float p3_1 = 0.4f;
+    float p3_2 = 0.3f;
+    float p3_1_rcp = 0.2f;
+    float p3_2_rcp = 0.11f;
+    float n3 = 8;
 
-	std::cout << "Waiting, unlimited, p_rcp:\t" << AvgInWaitingAlgorithm(0.8f, 100000, 0, 0.2f) << std::endl;
+	std::cout << "Waiting, unlimited, p_rcp:\t" << AvgInWaitingAlgorithm(p3_1, 100000, 0, p3_1_rcp) << std::endl;
+    std::cout << "Should be:\t\t" << 1 / ((1 - p3_1) * (1 - p3_1_rcp)) << std::endl;
 
-	std::cout << "Waiting, limited, p_rcp:\t" << AvgInWaitingAlgorithm(0.8f, 100000, 2, 0.2f) << std::endl;
+	std::cout << "Waiting, limited, p_rcp:\t" << AvgInWaitingAlgorithm(p3_2, 100000, n3, p3_2_rcp) << std::endl;
+    std::cout << "Should be:\t\t" << (1 - std::pow(p3_2, n3)) / ((1 - p3_2) * (1 - p3_2_rcp)) << std::endl;
+    /* sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss-sss */
+	float sum = 0;
+    for (int i = 1; i <= n3; i++)
+    {
+        sum += i * std::pow((1 - (1 - p3_2) * (1 - p3_2_rcp)), i - 1);
+    }
+    sum *= (1 - p3_2) * (1- p3_2_rcp);
+    sum += n3 * std::pow((1 - (1 - p3_2) * (1 - p3_2_rcp)), n3);
+    std::cout << "May be:\t\t" << sum << std::endl;
 
-	/* 2.4 Определение коэффициента использования канала при τ > 0 для алгоритма с ожиданием */
+    /* 2.4 Определение коэффициента использования канала при τ > 0 для алгоритма с ожиданием   `*/
 	float p4 = 0.5f;
 	float tau4 = 2;
 
@@ -191,11 +210,8 @@ int main()
 	std::cout << "Should be:\t\t" << (1 - p4) / (1 + tau4) << std::endl;
 
 	/* 2.5 Моделирование алгоритма с возвратом */
-
 	float p5 = 0.4f;
 	float tau5 = 3;
-	std::cout << "Usage koeff:\t\t" << UsageInReturnAlgorithm(p5, tau5, 1000) << std::endl;
+	std::cout << "Usage koeff:\t\t" << UsageInReturnAlgorithm(p5, tau5, 10) << std::endl;
 	std::cout << "Should be:\t\t" << (1 - p5) / (1 + p5 * tau5) << std::endl;
-
-
 }
---
+++ cmake-build-debug/CMakeFiles/Feedback.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Feedback.dir/Feedback/main.cpp.o"
+  "Feedback.pdb"
+  "Feedback"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Feedback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
